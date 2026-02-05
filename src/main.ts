98fe6d5413d9ebc7bf50e5357b94ea081c72e1c5
arobbins <arobbins@mitre.org>
2026-02-05 10:13:00 -0500

    upgrade primevue, add left sidebar on technique pages

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
 import { router } from "./router";
 import App from "./App.vue";
+import Aura from "@primevue/themes/aura"; // Import the Material preset
 import "./index.css";
 import PrimeVue from "primevue/config";
-import "primevue/resources/themes/mdc-light-deeppurple/theme.css";
-// import "bootstrap/dist/css/bootstrap.min.css"; // For Bootstrap styles
-// import "bootstrap"; // For Bootstrap's JavaScript functionality (requires Popper.js and jQuery)
 
-createApp(App).use(router).use(PrimeVue).mount("#app");
+createApp(App)
+  .use(router)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
+  .mount("#app");
--- src/views/TechniqueView.vue
+++ src/views/TechniqueView.vue
@@ -1,17 +1,34 @@
 <template>
-    <breadcrumb-component :breadcrumbItems="breadcrumbItems" />
-    <div>
-        <h1><span class="highlight">{{ technique.id }}</span> {{ technique.name }} <a v-if="technique.isAttack"
-                :href="'https://attack.mitre.org/techniques/' + technique.id + '/'" class="attack-indicator"
-                target="_blank">&</a></h1>
-        <h2>Description</h2>
-        <div class="markdown-html" v-html="renderedHtml(technique.description)"></div>
-        <template v-if="technique.subtechniques?.length > 0">
-            <h2>Subtechniques</h2>
-            <ul>
-                <li v-for="subtechnique in technique.subtechniques" :key="subtechnique">{{ subtechnique }}</li>
-            </ul>
-        </template>
+    <div class="flex h-full">
+        <div class="sidebar">
+            <Accordion multiple :value="sidenavValue">
+                <template v-for="tactic in tactics" :key="tactic.id">
+                    <AccordionPanel :value="tactic.id">
+                        <AccordionHeader> <router-link :to="'/tactic/' + tactic.id">{{ tactic.name }}</router-link>
+                        </AccordionHeader>
+                        <AccordionContent>list subtechniques here</AccordionContent>
+                    </AccordionPanel>
+                </template>
+            </Accordion>
+
+        </div>
+        <div class="main-container">
+            <breadcrumb-component :breadcrumbItems="breadcrumbItems" />
+            <div>
+                <h1><span class="highlight">{{ technique.id }}</span> {{ technique.name }} <a v-if="technique.isAttack"
+                        :href="'https://attack.mitre.org/techniques/' + technique.id + '/'" class="attack-indicator"
+                        target="_blank">&</a></h1>
+                <h2>Description</h2>
+                <div class="markdown-html" v-html="renderedHtml(technique.description)"></div>
+                <template v-if="technique.subtechniques?.length > 0">
+                    <h2>Subtechniques</h2>
+                    <ul>
+                        <li v-for="subtechnique in technique.subtechniques" :key="subtechnique">{{ subtechnique }}</li>
+                    </ul>
+                </template>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -20,9 +37,13 @@ import { defineComponent } from "vue";
 import json from "../data/matrix-data.json";
 import BreadcrumbComponent from "../components/BreadcrumbComponent.vue";
 import MarkdownIt from 'markdown-it';
+import Accordion from 'primevue/accordion';
+import AccordionPanel from 'primevue/accordionpanel';
+import AccordionHeader from 'primevue/accordionheader';
+import AccordionContent from 'primevue/accordioncontent';
 
 export default defineComponent({
-    components: { BreadcrumbComponent },
+    components: { BreadcrumbComponent, Accordion, AccordionContent, AccordionHeader, AccordionPanel },
     data() {
         return {
             matrixData: json,
@@ -31,6 +52,7 @@ export default defineComponent({
                 { label: "Techniques", route: "/techniques" },
                 { label: `${this.$route.params.id}`, route: `/technique/${this.$route.params.id}` }
             ],
+            sidenavValue: ['TA0003']
 
         };
     },
@@ -40,6 +62,9 @@ export default defineComponent({
         },
         technique() {
             return this.matrixData?.filter(i => i.id == this.techniqueId)[0]
+        },
+        tactics() {
+            return this.matrixData.filter(i => i.tactic)
         }
     },
     methods: {
@@ -53,5 +78,22 @@ export default defineComponent({
 <style scoped>
 .highlight {
     @apply text-ctid-blue font-medium
+}
+
+.main-container {
+    @apply w-3/4 p-8 px-12 h-full
+}
+
+.sidebar {
+    @apply w-1/4 block border-r-2 p-8 border-ctid-light-gray h-full
+}
+
+
+.sidebar h4 {
+    @apply text-ctid-gray font-medium
+}
+
+.sidebar a {
+    @apply text-ctid-blue hover:text-ctid-navy hover:underline
 }
 </style>
