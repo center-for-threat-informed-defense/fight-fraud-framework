cf235798de1d19517dec4dca9c7756b350a9333f
arobbins <arobbins@mitre.org>
2026-02-06 11:28:46 -0500

    add search to navbar

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -1,11 +1,11 @@
 <template>
   <nav class="navbar">
-    <div class="flex mx-8">
-      <router-link to="/" class="mr-auto my-auto w-max">
+    <div class="flex justify-between mx-8">
+      <router-link to="/" class="my-auto w-max">
         <h1>Fight Financial Fraud</h1>
       </router-link>
-      <div class="lg:inline-block hidden card">
-        <TabMenu :model="items" :active-index="getActiveIndex()">
+      <div class="lg:flex hidden w-max gap-10">
+        <TabMenu class="w-max" :model="items" :active-index="getActiveIndex()">
           <template #item="{ item, props }">
             <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
               <a :to="href" v-bind="props.action" @click="navigate">
@@ -14,6 +14,14 @@
             </router-link>
           </template>
         </TabMenu>
+        <InputGroup>
+          <InputText size="small" variant="outline" placeholder="Search" />
+          <InputGroupAddon>
+            <Button severity="secondary" variant="icon" @click="toggle">
+              <i class="pi pi-search"></i>
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
       </div>
       <div class=" lg:hidden inline-block w-max my-auto">
         <MenuBar :model="items" id="overlay_menu" ref="menu" class=" text-white z-50  p-menubar-mobile">
@@ -25,6 +33,7 @@
             </router-link>
           </template>
         </MenuBar>
+        <Menu ref="menu" :model="items" popup class="!min-w-fit" />
       </div>
     </div>
   </nav>
@@ -35,9 +44,13 @@ import { defineComponent } from "vue";
 import TabMenu from "primevue/tabmenu";
 import MenuBar from "primevue/menubar";
 import Logo from "@/assets/logo-line-white.svg";
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import InputText from 'primevue/inputtext';
+
 
 export default defineComponent({
-  components: { TabMenu, MenuBar },
+  components: { TabMenu, MenuBar, InputGroup, InputGroupAddon, InputText },
   data() {
     return {
       Logo,
@@ -72,5 +85,21 @@ export default defineComponent({
 
 .navbar h1 {
   @apply my-auto font-medium text-xl uppercase w-max
+}
+
+.p-inputgroup {
+  @apply w-80
+}
+
+.p-inputgroup {
+  @apply my-1
+}
+
+.p-inputtext-sm.p-inputfield-sm, .p-inputgroupaddon {
+  @apply bg-ctid-navy text-ctid-light-gray
+}
+
+.p-inputgroupaddon button {
+  @apply text-ctid-light-gray
 }
 </style>
