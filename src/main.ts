ba1e2951c22f3ad3d5862bc11254fa35961ba2a7
arobbins <arobbins@mitre.org>
2026-02-13 14:03:48 -0500

    tailwind presets

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { createApp } from "vue";
 import { router } from "./router";
 import App from "./App.vue";
-import Aura from "@primevue/themes/aura"; // Import the Material preset
+import Aura from "@primevue/themes/aura";
 import "./index.css";
 import PrimeVue from "primevue/config";
+import { semantic } from "@primeuix/themes/aura/base";
 
 createApp(App)
   .use(router)
   .use(PrimeVue, {
     theme: {
-      preset: Aura,
+      preset: {
+        ...Aura,
+        semantic: {
+          ...semantic,
+          primary: {
+            500: "#005B94",
+          },
+        },
+      },
     },
   })
   .mount("#app");
