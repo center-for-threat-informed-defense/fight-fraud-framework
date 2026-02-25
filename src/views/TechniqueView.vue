294f6340a3d24a7ce50c2d7940dfbbaa35a2f6e0
arobbins <arobbins@mitre.org>
2026-02-25 10:57:36 -0500

    conditional pluralization of titles

--- src/views/TechniqueView.vue
+++ src/views/TechniqueView.vue
@@ -10,7 +10,7 @@
                             <ul v-if="tactic.techniques">
                                 <li v-for="technique of tactic.techniques" :key="technique">
                                     <router-link :to="'/technique/' + technique">{{ getTechniqueData(technique).name
-                                    }}</router-link>
+                                        }}</router-link>
                                 </li>
                             </ul>
                         </AccordionContent>
@@ -20,49 +20,51 @@
         </div>
         <div class="main">
             <breadcrumb-component :breadcrumbItems="breadcrumbItems" />
-            <div>
-                <h1> {{ technique.name }} <a v-if="technique.isAttack"
-                        :href="'https://attack.mitre.org/techniques/' + technique.id + '/'" class="attack-indicator"
-                        target="_blank">&</a></h1>
-                <h2>Description</h2>
-                <div class="markdown-html" v-html="renderedHtml(technique.description)"></div>
-                <!-- for tactics: display list of techniques under the tactic -->
-                <template v-if="technique.tactic && technique.techniques.length > 0">
-                    <h2>Technique<template v-if="technique.techniques.length > 1">s</template></h2>
-                    <ul>
-                        <li v-for="t in technique.techniques" :key="t">
-                            <router-link :to="'/technique/' + t">{{ getTechniqueData(t).name
-                                }}</router-link>
-                        </li>
-                    </ul>
-                </template>
+            <h1> {{ technique.name }} <a v-if="technique.isAttack"
+                    :href="'https://attack.mitre.org/techniques/' + technique.id + '/'" class="attack-indicator"
+                    target="_blank">&</a></h1>
+            <h2>Description</h2>
+            <div class="markdown-html" v-html="renderedHtml(technique.description)"></div>
+            <!-- for tactics: display list of techniques under the tactic -->
+            <template v-if="technique.tactic && technique.techniques.length > 0">
+                <h2>Technique<template v-if="technique.techniques.length > 1">s</template></h2>
+                <ul>
+                    <li v-for="t in technique.techniques" :key="t">
+                        <router-link :to="'/technique/' + t">{{ getTechniqueData(t).name
+                            }}</router-link>
+                    </li>
+                </ul>
+            </template>
 
-                <!-- if there are subtechniques, list them -->
-                <template v-if="technique.subtechniques?.length > 0">
-                    <h2>Subtechniques</h2>
-                    <ul>
-                        <li v-for="subtechnique in technique.subtechniques" :key="subtechnique">
-                            <router-link :to="'/technique/' + subtechnique">{{ getTechniqueData(subtechnique).name
-                                }}</router-link>
-                        </li>
-                    </ul>
-                </template>
-                <!-- for techniques, display parent tactic -->
-                <template v-if="!technique.tactic">
-                    <h2>Tactics</h2>
-                    <ul>
-                        <li v-for="tactic in parentTactic" :key="tactic">
-                            <router-link :to="'/tactic/' + tactic.id">{{ tactic.name
-                                }}</router-link>
-                        </li>
-                    </ul>
-                </template>
-                <div class="mt-8">
-                    <router-link :to="'/contact-us/update-technique/' + techniqueId">Suggest improvements to
-                        this <template v-if="technique.tactic">tactic</template>
-                        <template v-else>technique</template>
-                    </router-link>
-                </div>
+            <!-- if there are subtechniques, list them -->
+            <template v-if="technique.subtechniques?.length > 0">
+                <h2>Subtechnique<template v-if="technique.subtechniques?.length > 1">s</template></h2>
+                <ul>
+                    <li v-for="subtechnique in technique.subtechniques" :key="subtechnique">
+                        <router-link :to="'/technique/' + subtechnique">{{ getTechniqueData(subtechnique).name
+                            }}</router-link>
+                    </li>
+                </ul>
+            </template>
+            <!-- for techniques, display parent tactic -->
+            <template v-if="!technique.tactic">
+                <h2>Tactic<template v-if="parentTactic.length > 1">s</template></h2>
+                <ul>
+                    <li v-for="tactic in parentTactic" :key="tactic">
+                        <router-link :to="'/tactic/' + tactic.id">{{ tactic.name
+                            }}</router-link>
+                    </li>
+                </ul>
+            </template>
+            <div class="mt-8">
+                <i class="pi pi-send text-ctid-gray mx-2"></i>
+                <router-link :to="'/contact-us/update-technique/' + techniqueId">Suggest improvements to
+                    this <template v-if="technique.tactic">tactic</template>
+                    <template v-else>technique</template>
+                </router-link>
+            </div>
+            <div>
+
             </div>
         </div>
         <div class="right-sidebar">
@@ -72,27 +74,28 @@
                 {{ technique.id }}
             </p>
 
-            <p v-if="technique.tactic && technique.techniques" class="sidebar-item">
-                <span class="emphasis">Techniques: </span>
+            <p v-if="technique.tactic && technique.techniques.length > 0" class="sidebar-item">
+                <span class="emphasis">Technique<template v-if="technique.techniques.length > 1">s</template>: </span>
                 <template v-for="(t, i) in technique.techniques" :key="t">
                     <router-link :to="'/technique/' + t">{{ getTechniqueData(t).name
-                        }}</router-link>
+                    }}</router-link>
                     <span v-if="i < technique.techniques.length - 1">, </span>
                 </template>
             </p>
             <p v-if="technique.subtechniques?.length > 0" class="sidebar-item">
-                <span class="emphasis">Subtechniques: </span>
+                <span class="emphasis">Subtechnique<template v-if="technique.subtechniques?.length > 1">s</template>:
+                </span>
                 <template v-for="(t, i) in technique.subtechniques" :key="t">
                     <router-link :to="'/technique/' + t">{{ getTechniqueData(t).name
-                        }}</router-link>
+                    }}</router-link>
                     <span v-if="i < technique.subtechniques.length - 1">, </span>
                 </template>
             </p>
             <p v-if="!technique.tactic" class="sidebar-item">
-                <span class="emphasis">Tactics: </span>
+                <span class="emphasis">Tactic<template v-if="parentTactic.length > 1">s</template>: </span>
                 <template v-for="(tactic, i) in parentTactic" :key="tactic">
                     <router-link :to="'/tactic/' + tactic.id">{{ tactic.name
-                        }}</router-link>
+                    }}</router-link>
                     <span v-if="i < parentTactic.length - 1">, </span>
                 </template>
             </p>
@@ -186,9 +189,6 @@ li {
     @apply text-ctid-red
 }
 
-.right-sidebar {
-    @apply border-l-2 border-ctid-light-gray pl-6 ml-6 w-1/4
-}
 
 .sidebar-item {
     @apply my-4
