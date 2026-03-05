cd65276c5faa5f6edf4e578f5fe6783bea85bad0
arobbins <arobbins@mitre.org>
2026-03-05 15:45:41 -0500

    update with corrected spreadsheet

--- src/views/DesignPrinciplesView.vue
+++ src/views/DesignPrinciplesView.vue
@@ -14,7 +14,7 @@
       <breadcrumb-component :breadcrumbItems="breadcrumbItems" class="mb-2 -ml-3" />
 
       <h1>Design Principles and Methodology </h1>
-      <h2 id="introduction">Introduction</h2>
+      <!-- <h2 id="introduction">Introduction</h2>
       <p>The Fight Financial Fraud (F3) Framework is a curated knowledge base of tactics, techniques, and procedures
         (TTPs) used by fraud actors. The F3 Framework includes behaviors that characterize known fraud TTPs and
         references existing <a href="https://attack.mitre.org/">MITRE ATT&CK®</a> cyber techniques as applicable to
@@ -85,7 +85,7 @@
           <p>Framework to enhance understanding of the tactics, techniques, and procedures (TTPs) used by actors in
             fraudulent activities </p>
         </div>
-      </div>
+      </div> -->
       <p>The F3 Framework is grounded in real-world fraud actor behavior derived from the as and developed by fraud
         fusion analysts. The framework is modeled after ATT&CK, inspired by the MITRE ATT&CK®: Design and Philosophy2,
         to enumerate fraud actor behavior in banking institutions. The F3 Framework includes new content to describe
@@ -93,6 +93,18 @@
         ATT&CK techniques when they are applicable to financial fraud. </p>
 
       <h2 id="design-principles">Design Principles</h2>
+      <p>The F3 Framework was developed using a set of clear design principles, informed by the practical experience of
+        fraud fusion analysts and applied to define its tactics, techniques, and sub-techniques. These design principles
+        form the conceptual foundation of the methodology used to create the F3 Framework and will continue to guide its
+        maintenance and future enhancement.
+      </p>
+      <p>
+        Fraud methods will evolve over time and additional knowledge will be gained. The F3 development process will
+        remain focused on accurately representing how fraud actors operate, through following these design principles
+        and providing information in a way that makes it easy to categorize fraud actor actions and relate those actions
+        to defenses. This enables defenders to consistently describe, detect, and prevent fraud incidents using a
+        common, structured approach. </p>
+      <p>There are four Design Principles that are core to the development of the F3 Framework: </p>
       <div class="design-principle">
         <h3>Design Principle #1: The financial institution must see the effects of a technique during the fraud
           incident.
@@ -129,6 +141,46 @@
         <p>Sub-techniques allow for a similar abstraction level of techniques across the framework, while demonstrating
           the many ways that techniques are performed. </p>
       </div>
+      <h2>Design Principle #1: The financial institution must see the effects of a technique during the fraud incident.
+      </h2>
+      <p>The banking institution must be able to observe and understand how a technique impacts a fraud incident. Seeing
+        how the behavior or action fits in the overall outcome of the fraud incident is needed so that its
+        effectiveness, limitations, and side effects can be accurately evaluated and used to improve future strategies
+        for mitigating and detecting the behavior. </p>
+      <p>Visibility into what the fraud actor does during each step of a fraud incident provides a clear understanding
+        of their methods and behaviors. This insight can then be used to inform and shape future fraud strategies,
+        rules, and processes, helping organizations better prevent, detect, and respond to similar threats in the
+        future. </p>
+      <h2>Design Principle #2: The fraud incident must contain a cyber-based technique. </h2>
+      <p>The fraud incident must involve the use of a cyber-based technique as an element of how the fraud is carried
+        out. This means the fraud incident includes use of digital or technological methods, such as phishing, malware,
+        unauthorized access to information systems, or other forms of cyber manipulation, rather than being solely
+        physical, social, or paper-based in nature. The requirement to contain a cyber artifact is for analysts to use
+        to help detect future fraud attempts. </p>
+      <p>The purpose is to provide actionable support for cyber threat intelligence, detection engineering, and security
+        control implementation, and performing investigations. This ensures that insights are not only understood but
+        can be effectively applied to strengthen an organization's overall security posture. </p>
+      <h2>Design Principle #3: Techniques must describe the behavior of the adversary. </h2>
+      <p>Techniques represent 'how' a fraud actor achieves a tactical goal by performing an action. Each technique
+        captures a distinct, observable action or behavior that is used by the fraud actor to achieve their objective. A
+        technique is not an entity or a tool, but rather the specific way those entities or tools are used. Techniques
+        focus on actions known to be used by threat actors, represented by standardized terms. </p>
+      <p>Defining techniques in this way enables clear recognition and consistent categorization of fraud behaviors
+        within an incident. This also provides operational value by ensuring that defenses such as detection logic and
+        security control design can be mapped to the actions performed by fraud actors, rather than to the specific
+        entities or tools used to achieve their goals. </p>
+      <h2>Design Principle #4: For behaviors with the same "how” performed in different ways, we must create a
+        technique/sub-technique relationship. </h2>
+      <p>Not all techniques will have sub-techniques. Sub-techniques describe more specific means by which fraud actors
+        achieve tactical goals at a lower level than techniques. Sub-techniques further break down behaviors described
+        by techniques into more specific descriptions of how behavior is used to achieve an objective. </p>
+      <p>Techniques provide a high-level, manageable view of fraud behavior, while sub-techniques provide the additional
+        detail needed for demonstrating the many ways that techniques are performed. This structure helps to reduce
+        overlap and allows for a similar abstraction level of techniques across the framework, capturing variations and
+        lower-level details as sub-techniques. </p>
+
+
+
     </div>
   </div>
 </template>
@@ -170,5 +222,9 @@ blockquote {
 
 .design-principle h3 {
   @apply font-medium
+}
+
+.subtitle {
+  @apply font-medium italic
 }
 </style>
